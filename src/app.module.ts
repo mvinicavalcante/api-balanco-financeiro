3c7d53c10ffe7afed205560b6545196a1d6f1800
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2024-05-11 17:12:56 -0300

    chore: init config auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseConfigModule } from './config/mongoose';
 import { SchemasModule } from './schemas/schemas.module';
 import { ModulesModule } from './modules/modules.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ModulesModule } from './modules/modules.module';
     MongooseConfigModule,
     SchemasModule,
     ModulesModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
