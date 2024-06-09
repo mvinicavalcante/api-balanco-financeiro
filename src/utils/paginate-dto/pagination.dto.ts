5f7971d423de09f4567aebde01c7a89610faa6d0
Marcos Vinícius Cavalcante <mvinic2011@gmail.com>
2024-06-08 22:41:22 -0300

    feat: done

--- src/utils/paginate-dto/pagination.dto.ts
+++ src/utils/paginate-dto/pagination.dto.ts
@@ -1,27 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { isNumberString, IsOptional, Min } from 'class-validator';
+import { IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationDTO {
-  @ApiProperty({
-    description: 'Page number',
-    required: false,
-    type: Number,
-    default: 1,
-  })
+  @ApiProperty({ required: false, type: Number, default: 0 })
   @IsOptional()
   @Min(0)
-  @Transform(({ value }) => (isNumberString(value) ? Number(value) : undefined))
+  @Transform(({ value }) => (value ? Number(value) : undefined))
   page = 0;
 
-  @ApiProperty({
-    description: 'Number of items per page',
-    required: false,
-    type: Number,
-    default: 10,
-  })
+  @ApiProperty({ required: false, type: Number, default: 50 })
   @IsOptional()
   @Min(0)
-  @Transform(({ value }) => (isNumberString(value) ? Number(value) : undefined))
-  limit = 15;
+  @Max(250)
+  @Transform(({ value }) => (value ? Number(value) : undefined))
+  limit = 50;
+}
+
+export class PaginationResponseDTO {
+  @ApiProperty({ type: Number, required: true })
+  total: number;
+
+  @ApiProperty({ type: Number, required: true })
+  page: number;
+
+  @ApiProperty({ type: Number, required: true })
+  limit: number;
 }
